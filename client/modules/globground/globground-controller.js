3fb77dc7320805db3b164824ab885af0cc47b693
Kentaro Wakayama <wakayama.kentaro@gmail.com>
2014-05-28 09:43:00 +0200

    fix jshint errors

--- client/modules/globground/globground-controller.js
+++ client/modules/globground/globground-controller.js
@@ -14,60 +14,71 @@ angular.module('koan.globground').controller('GlobgroundCtrl', function($scope, 
   };
 
   $scope.dataForTheTree = [{
-      "name": ".DS_Store",
-      "path": ".DS_Store",
-      "type": "file"
+      'name': '.DS_Store',
+      'path': '.DS_Store',
+      'type': 'file'
   }, {
-      "name": ".hiddenfile",
-      "path": ".hiddenfile",
-      "type": "file"
+      'name': '.hiddenfile',
+      'path': '.hiddenfile',
+      'type': 'file'
   }, {
-      "name": "img",
-      "type": "folder",
-      "path": "img",
-      "content": [{
-          "name": ".DS_Store",
-          "path": "img/.DS_Store",
-          "type": "file"
+      'name': 'img',
+      'type': 'folder',
+      'path': 'img',
+      'content': [{
+          'name': '.DS_Store',
+          'path': 'img/.DS_Store',
+          'type': 'file'
       }, {
-          "name": "logo.png",
-          "path": "img/logo.png",
-          "type": "file"
+          'name': 'logo.png',
+          'path': 'img/logo.png',
+          'type': 'file'
       }, {
-          "name": "user.gif",
-          "path": "img/user.gif",
-          "type": "file"
+          'name': 'user.gif',
+          'path': 'img/user.gif',
+          'type': 'file'
       }]
   }, {
-      "name": "index.js",
-      "path": "index.js",
-      "type": "file"
+      'name': 'index.js',
+      'path': 'index.js',
+      'type': 'file'
   }, {
-      "name": "cli.js",
-      "path": "cli.js",
-      "type": "file"
+      'name': 'cli.js',
+      'path': 'cli.js',
+      'type': 'file'
   }, {
-      "name": "package.json",
-      "path": "package.json",
-      "type": "file"
+      'name': 'package.json',
+      'path': 'package.json',
+      'type': 'file'
   }, {
-      "name": "src",
-      "type": "folder",
-      "path": "src",
-      "content": [{
-          "name": ".DS_Store",
-          "path": "src/.DS_Store",
-          "type": "file"
+      'name': 'src',
+      'type': 'folder',
+      'path': 'src',
+      'content': [{
+          'name': '.DS_Store',
+          'path': 'src/.DS_Store',
+          'type': 'file'
       }, {
-          "name": "user.js",
-          "path": "src/user.js",
-          "type": "file"
+          'name': 'user.js',
+          'path': 'src/user.js',
+          'type': 'file'
       }, {
-          "name": "utils.js",
-          "path": "src/utils.js",
-          "type": "file"
+          'name': 'utils.js',
+          'path': 'src/utils.js',
+          'type': 'file'
       }]
   }];
+
+  var flattenFileTree = function(data) {
+    var result = [];
+    data.forEach(function(item) {
+      result.push(item.path);
+      if(item.content) {
+        result = result.concat(flattenFileTree(item.content));
+      }
+    });
+    return result;
+  };
 
   // add post/comment creation functions to scope
   $scope.submitGlob = function($event) {
@@ -80,16 +91,6 @@ angular.module('koan.globground').controller('GlobgroundCtrl', function($scope, 
     $location.search('glob', $scope.globPattern);
     $location.search('hidden', $scope.options.hidden);
 
-    var flattenFileTree = function(data) {
-      var result = [];
-      data.forEach(function(item) {
-        result.push(item.path);
-        if(item.content) {
-          result = result.concat(flattenFileTree(item.content));
-        }
-      });
-      return result;
-    }
     var files = flattenFileTree($scope.dataForTheTree);
     $scope.globResult =  minimatch.match(files, $scope.globPattern, $scope.options);
     setSelectedClass($scope.dataForTheTree);
@@ -100,7 +101,7 @@ angular.module('koan.globground').controller('GlobgroundCtrl', function($scope, 
     data.forEach(function(item) {
 
       var matchItem = _.findWhere($scope.globResult, function(globItem) {
-        return globItem == item.path;
+        return globItem === item.path;
       });
 
       if(matchItem) {
